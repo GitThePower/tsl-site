88f70cfeb40a93956d54dd73141515996a4753ff
GitThePower <henrypower11@gmail.com>
2024-04-03 21:06:55 -0400

    Adding login button to home screen that takes user to login page

---
+++ frontend/src/components/LoginButton.tsx
@@ -0,0 +1,18 @@
+import { Button } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+
+const LoginButton = () => {
+  const navigate = useNavigate();
+
+  const handleLoginButtonClick = () => {
+    navigate('/login');
+  };
+
+  return (
+    <Button variant="contained" color="primary" onClick={handleLoginButtonClick}>
+      Login
+    </Button>
+  );
+}
+
+export default LoginButton;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,30 +1,32 @@
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+} from '@mui/material';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import logo from './assets/vite.svg';
-
-import Login from './pages/login'
-import Home from './pages/home'
+import LoginButton from './components/LoginButton';
+import Home from './pages/home';
+import Login from './pages/login';
+import './main.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <div>
+  <BrowserRouter>
     <header>
       <AppBar position="static">
-        <Toolbar>
-          <img src={logo} alt="Your Logo" style={{ height: '40px', marginRight: '10px' }} />
+        <Toolbar className='header-toolbar'>
+          <img src={logo} alt="Your Logo" className='header-toolbar-logo' />
           <Typography variant="h6">TSL Site</Typography>
+          <LoginButton />
         </Toolbar>
       </AppBar>
     </header>
     <main>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </main>
-  </div>
+  </BrowserRouter>
 )
